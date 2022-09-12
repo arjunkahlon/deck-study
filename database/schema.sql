abcb5b027d7f222642513c8fde08d60697175ccf
Arjun Kahlon <arjunskahlon@gmail.com>
2022-09-12 22:44:41 +0000

    Initialized database of application. Created database schema file and imported starting data from data.sql. Updated index.js to require postgresql package. Instantiated new PostgreSQL connection pool to database.

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,47 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+ CREATE TABLE "public"."users" (
+	"userId" serial NOT NULL,
+	"username" TEXT NOT NULL UNIQUE,
+	"hashedPassword" TEXT NOT NULL,
+	"joinedAt" timestamptz NOT NULL default now(),
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."decks" (
+	"deckId" serial NOT NULL,
+	"deckName" TEXT NOT NULL,
+	"userId" int NOT NULL,
+	"createdAt" timestamptz NOT NULL default now(),
+	"deckStudyCount" int NOT NULL,
+	CONSTRAINT "decks_pk" PRIMARY KEY ("deckId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public"."cards" (
+	"cardId" serial NOT NULL,
+	"deckId" int NOT NULL,
+	"question" TEXT NOT NULL,
+	"answer" TEXT NOT NULL,
+	"difficulty" int NOT NULL,
+	"createdAt" timestamptz NOT NULL default now(),
+	CONSTRAINT "cards_pk" PRIMARY KEY ("cardId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "decks" ADD CONSTRAINT "decks_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
+
+ALTER TABLE "cards" ADD CONSTRAINT "cards_fk0" FOREIGN KEY ("deckId") REFERENCES "decks"("deckId");
--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
+/* eslint-disable no-unused-vars */
+
 require('dotenv/config');
+const pg = require('pg');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
 
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 const app = express();
+
+const jsonMiddleWare = express.json();
+app.use(jsonMiddleWare);
 
 app.use(staticMiddleware);
 
