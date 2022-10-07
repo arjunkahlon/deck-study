cfc6ccc8768a1952f8f0bffca35b781e5e80a03c
Arjun Kahlon <arjunskahlon@gmail.com>
2022-10-07 00:52:01 +0000

    Updated data.sql with an insertion of demo user account. Updated AuthForm component to autofill the username as demo rather than devUser.

--- database/data.sql
+++ database/data.sql
@@ -1,2 +1,2 @@
 insert into "users" ("username", "hashedPassword")
-values ('devUser', '$argon2i$v=19$m=4096,t=3,p=1$h7icQD/xZr8akZsX+hNA0A$h68atJWyjvunAwNOpSpMfg9sPvoMQ6dKwoh0dJhurWA');
+values ('demo', '$argon2i$v=19$m=4096,t=3,p=1$h7icQD/xZr8akZsX+hNA0A$h68atJWyjvunAwNOpSpMfg9sPvoMQ6dKwoh0dJhurWA');
